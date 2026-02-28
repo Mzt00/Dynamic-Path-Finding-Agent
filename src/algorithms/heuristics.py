2b3546b2c96da2e74029457cc7252c7bc28ae27e
Muhammad Zain <mztahir006@gmail.com>
2026-03-01 00:10:35 +0500

    Added manhattan and euclidean distance in heuristics.py

--- src/algorithms/heuristics.py
+++ src/algorithms/heuristics.py
@@ -1,1 +1,11 @@
-
+import math
+def manhattan(a,b):
+    #mod(x1-x2) + mod(y1-2)
+    (x1,y1) = a
+    (x2,y2) = b
+    return abs(x1,x2) + abs(y1-y2)
+def euclidean(a,b):
+    #sqrt((x1-x2)^2 + (y1-y2)^2)
+    (x1,y1) = a
+    (x2,y2) = b
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
