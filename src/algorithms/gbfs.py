fd9bba06fc62f34dc4bd255146dcc18be66b7e61
Muhammad Zain <mztahir006@gmail.com>
2026-02-28 22:37:24 +0500

    Fixed Indentation gbfs.py

--- src/algorithms/gbfs.py
+++ src/algorithms/gbfs.py
@@ -32,9 +32,9 @@ def gbfs_search(grid,start,goal,heuristic_ref):
         if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
             cost_so_far = new_cost
             #only check the heuristic
-        priority = heuristic_func(next_node, goal)
-        frontier.put(next_node,priority)
-        came_from[next_node] = current
+            priority = heuristic_func(next_node, goal)
+            frontier.put(next_node,priority)
+            came_from[next_node] = current
     execution_time = (time.time() - start_time)*1000
     path = reconstruct_path(came_from,start,goal)
     return {
