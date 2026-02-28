5242ff78173223b64bf898c04afc0af18208355e
Muhammad Zain <mztahir006@gmail.com>
2026-03-01 03:49:21 +0500

    Fixed GUI problems

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ def main():
     start = (2, 2)
     goal = (rows - 2, cols - 2)
     agent = Agent(start)
-
     current_algo = a_star_search
     current_heuristic = manhattan
+    selected_algo = "A*"
+    selected_distance = "Manhattan"
     visited = []
     dynamic_mode = False
     clock = pygame.time.Clock()
@@ -43,17 +44,11 @@ def main():
                 for name, rect in renderer.buttons.items():
                     if rect.collidepoint(pos):
 
-                        if name == "A*":
-                            current_algo = a_star_search
+                        if name in ["A*", "GBFS"]:
+                            selected_algo = name
 
-                        elif name == "GBFS":
-                            current_algo = gbfs_search
-
-                        elif name == "Manhattan":
-                            current_heuristic = manhattan
-
-                        elif name == "Euclidean":
-                            current_heuristic = euclidean
+                        elif name in ["Manhattan", "Euclidean"]:
+                            selected_distance = name
 
                         elif name == "Dynamic":
                             dynamic_mode = not dynamic_mode
@@ -62,6 +57,20 @@ def main():
                             grid.walls.clear()
                             grid.nodes = [[0 for _ in range(cols)] for _ in range(rows)]
                             visited, agent.pos, agent.path, agent.is_moving = [], start, [], False
+
+                        elif name == "Start" and not agent.is_moving:
+
+                            algo_func = a_star_search if selected_algo == "A*" else gbfs_search
+                            heuristic = manhattan if selected_distance == "Manhattan" else euclidean
+
+                            temp = algo_func(grid, agent.pos, goal, heuristic)
+                            agent.set_path(temp["path"])
+                            visited = temp["visited"]
+                            agent.is_moving = True
+
+                            metrics["visited"] = len(temp["visited"])
+                            metrics["cost"] = temp["cost"]
+                            metrics["time"] = temp["time"]
 
                 c = pos[0] // renderer.cell_size
                 r = pos[1] // renderer.cell_size
@@ -75,57 +84,25 @@ def main():
                 if grid.is_in_bounds((r, c)) and (r, c) not in [start, goal]:
                     grid.toggle_walls((r, c))
 
-            #KEYBOARD CONTROLS
-            # if event.type == pygame.KEYDOWN:
-            #     if event.key == pygame.K_1: current_algo = a_star_search
-            #     if event.key == pygame.K_2: current_algo = gbfs_search
-            #     if event.key == pygame.K_m: current_heuristic = manhattan
-            #     if event.key == pygame.K_e: current_heuristic = euclidean
-            #     if event.key == pygame.K_d: dynamic_mode = not dynamic_mode
-
-            #     if event.key == pygame.K_r:
-            #         grid.generate_random_walls(density=0.25)
-            #         visited, agent.pos, agent.path, agent.is_moving = [], start, [], False
-
-            #     if event.key == pygame.K_c:
-            #         grid.walls.clear()
-            #         grid.nodes = [[0 for _ in range(cols)] for _ in range(rows)]
-            #         visited, agent.pos, agent.path, agent.is_moving = [], start, [], False
-
-            #     if event.key == pygame.K_SPACE:
-            #         if not agent.is_moving:
-            #             temp = current_algo(grid, agent.pos, goal, current_heuristic)
-            #             agent.set_path(temp["path"])
-            #             visited = temp["visited"]
-            #             agent.is_moving = True
-
-            #             metrics["visited"] = len(temp["visited"])
-            #             metrics["cost"] = temp["cost"]
-            #             metrics["time"] = temp["time"]
+        reached_goal = False
 
         if agent.is_moving:
             reached_goal = agent.move()
 
-            if dynamic_mode:
-                is_blocked, obs, pos = dynamic_obstacles(grid, agent.path, agent.pos, goal, rows, cols)
+        if dynamic_mode:
+            is_blocked, obs, pos = dynamic_obstacles(grid, agent.path, agent.pos, goal, rows, cols)
 
-                if is_blocked:
-                    new_plan = current_algo(grid, agent.pos, goal, current_heuristic)
-                    agent.set_path(new_plan["path"])
-                    visited.extend(new_plan["visited"])
+            if is_blocked:
+                new_plan = current_algo(grid, agent.pos, goal, current_heuristic)
+                agent.set_path(new_plan["path"])
+                visited.extend(new_plan["visited"])
 
-            if reached_goal:
-                agent.is_moving = False
+        if reached_goal:
+            agent.is_moving = False
 
         renderer.draw_grid(grid, path=agent.path, visited=visited)
 
-        selected = "A*" if current_algo == a_star_search else \
-                "GBFS" if current_algo == gbfs_search else \
-                "Manhattan" if current_heuristic == manhattan else \
-                "Euclidean" if current_heuristic == euclidean else \
-                "Dynamic" if dynamic_mode else "Static"
-
-        renderer.draw_panel(selected)
+        renderer.draw_panel(selected_algo, selected_distance, dynamic_mode)
 
         agent.draw(renderer.screen, renderer.cell_size)
 
--- src/core/dynamic.py
+++ src/core/dynamic.py
@@ -1,12 +1,11 @@
 import random
+
 def dynamic_obstacles(grid, current_path, agent_pos, goal, rows, cols, spawn_chance=0.10):
     if random.random() < spawn_chance:
         obs = (random.randint(0, rows-1), random.randint(0, cols-1))
         if obs != agent_pos and obs != goal and obs not in grid.walls:
-            grid.toggle_wall(obs)
+            grid.toggle_walls(obs)
             if obs in current_path:
-                return True, obs
-            return False, obs
-            
-    return False, None
+                return True, obs, agent_pos
 
+    return False, None, agent_pos
